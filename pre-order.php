411f6cc27a276c9e8cc7438e10335cc254fd0ab4
MrGKanev <contact@mrgkanev.eu>
2024-07-17 15:44:00 +0300

    Update pre-order.php

--- pre-order.php
+++ pre-order.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: WooCommerce Pre-Order Plugin
- * Description: Adds a Pre-Order page in the WooCommerce tab and checks for promo codes on specific products.
+ * Description: Adds a Pre-Order page in the WooCommerce tab and checks for a specific promo code on specific products.
  * Version: 1.0
  * Author: Your Name
  * Text Domain: wc-preorder-plugin
@@ -64,6 +64,14 @@ function wc_preorder_admin_init()
         'wc-preorder',
         'wc_preorder_main_section'
     );
+
+    add_settings_field(
+        'wc_preorder_promo_code',
+        __('Promo Code', 'wc-preorder-plugin'),
+        'wc_preorder_promo_code_field',
+        'wc-preorder',
+        'wc_preorder_main_section'
+    );
 }
 
 function wc_preorder_section_text()
@@ -78,36 +86,42 @@ function wc_preorder_product_ids_field()
     echo '<input type="text" id="wc_preorder_product_ids" name="wc_preorder_settings[product_ids]" value="' . esc_attr($product_ids) . '" />';
 }
 
+function wc_preorder_promo_code_field()
+{
+    $options = get_option('wc_preorder_settings');
+    $promo_code = isset($options['promo_code']) ? $options['promo_code'] : '';
+    echo '<input type="text" id="wc_preorder_promo_code" name="wc_preorder_settings[promo_code]" value="' . esc_attr($promo_code) . '" />';
+}
+
 // Hook into WooCommerce cart validation and checkout page to hide the proceed button
 add_action('woocommerce_check_cart_items', 'check_product_promo_code_and_hide_checkout_button');
 add_action('woocommerce_before_checkout_form', 'check_product_promo_code_and_hide_checkout_button');
 
 function check_product_promo_code_and_hide_checkout_button()
 {
-    // Check if a promo code is applied
+    // Get the promo code from settings
+    $options = get_option('wc_preorder_settings');
+    $required_promo_code = isset($options['promo_code']) ? $options['promo_code'] : '';
+
+    // Check if the required promo code is applied
     $applied_coupons = WC()->cart->get_applied_coupons();
-    if (empty($applied_coupons)) {
+    if (!in_array($required_promo_code, $applied_coupons)) {
         // Get the product IDs that require a promo code from the settings
-        $options = get_option('wc_preorder_settings');
         $product_ids_string = isset($options['product_ids']) ? $options['product_ids'] : '';
         $required_product_ids = array_map('trim', explode(',', $product_ids_string));
 
         // Check if any of the required product IDs are in the cart
         $found_required_product = false;
-        $required_product_id = null;
         foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
             if (in_array($cart_item['product_id'], $required_product_ids)) {
                 $found_required_product = true;
-                $required_product_id = $cart_item['product_id'];
                 break;
             }
         }
 
-        // If a required product is found and no promo code is applied, display an error message
+        // If a required product is found and the required promo code is not applied, display an error message
         if ($found_required_product) {
-            // Get the product name dynamically using the product ID
-            $product_name = get_the_title($required_product_id);
-            $error_message = sprintf('To continue, please apply a promo code for %s.', $product_name);
+            $error_message = 'To continue, please apply the required promo code.';
             wc_add_notice($error_message, 'error');
 
             // Hide the Proceed to Checkout button
